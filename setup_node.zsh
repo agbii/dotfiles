32de5e47e00c65c77f15b8ef2ee1c5a0e2e883bb
Eunbi Lee <10401133+agbii@users.noreply.github.com>
2021-09-11 02:46:01 +0900

    Add setup_node.zsh and matching Dotbot command
    Installs via n.

---
+++ setup_node.zsh
@@ -0,0 +1,11 @@
+#!/usr/bin/env zsh
+
+echo "\n<<< Starting Node Setup>>>\n"
+
+if exists node; then
+  echo "<<Node $(node --version) & NPM $(npm --version) already installed>>"
+else
+  echo "<<Installing Node & NPM with n...>>"
+  n latest
+  n lts
+fi
--- setup_zsh.zsh
+++ setup_zsh.zsh
@@ -5,21 +5,21 @@ echo "\n<<< Starting ZSH Setup>>>\n"
 # Installation unneccessary; it's in the Brewfile.
 
 if grep -Fxq '/opt/homebrew/bin/zsh' '/etc/shells'; then
-  echo '/opt/homebrew/bin/zsh already exists in /etc/shells'
+  echo '<</opt/homebrew/bin/zsh already exists in /etc/shells>>'
 else
   # echo "Enter superuse (sudo) password to edit /etc/shells"
   echo '/opt/homebrew/bin/zsh' | sudo tee -a '/etc/shells' >/dev/null
 fi
 
 if [ "$SHELL" = '/opt/homebrew/bin/zsh' ]; then
-  echo '$SHELL is already /opt/homebrew/bin/zsh'
+  echo '<<$SHELL is already /opt/homebrew/bin/zsh>>'
 else
   echo "Enter user password to change login shell"
   chsh -s '/opt/homebrew/bin/zsh'
 fi
 
 if sh --version | grep -q zsh; then
-  echo 'private/var/select/sh already linked to /bin/zsh'
+  echo '<<private/var/select/sh already linked to /bin/zsh>>'
 else
   echo "Enter superuser (sudo) password to symlink sh to zsh"
   # Linked to the original zsh because brew zsh can't be used
