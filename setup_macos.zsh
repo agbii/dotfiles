b3600ec9f03709b65651c51c25b66da6dd6e76ff
Eunbi Lee <10401133+agbii@users.noreply.github.com>
2021-09-11 20:50:00 +0900

    Turn on Three Finger Dragging for Trackpad
    The GUI doesn't update; "enable dragging" is still unchecked and the dropdown wasn't changed to "three finger drag."

--- setup_macos.zsh
+++ setup_macos.zsh
@@ -4,14 +4,12 @@ echo "\n<<< Starting macOS Setup>>>\n"
 
 osascript -e 'tell application "System Preferences" to quit'
 
-
 # Finder > View > Show Path Bar
 defaults write com.apple.finder ShowPathbar -bool true
 
 # Finder > Preferences > General > New Finder windows show:
 defaults write com.apple.finder NewWindowTarget -string 'PfLo'
 defaults write com.apple.finder NewWindowTargetPath -string "file://$HOME/.dotfiles"
-
 
 # System Preferences > Dock
 defaults write com.apple.dock magnification -bool true
@@ -21,7 +19,12 @@ defaults write com.apple.dock largesize -int 35
 #defaults write com.apple.dock autohide-time-modifier -float 0.25
 #defaults write com.apple.dock autohide-delay -float 0.1
 
+# System Preferences > Accessibility > Pointer Control > Mouse & Trackpad > Trackpad Options > Enable Dragging > Three Finger Drag (NOTE: The GUI doesn't update)
+defaults write com.apple.AppleMultitouchTrackpad TrackpadThreeFingerDrag -bool true
+
+
 # Finish macOS Setup
 killall Finder
 killall Dock
-echo "\n<<< macOS Setup Complete >>>\n"
+echo "\n<<< macOS Setup Complete.
+    A logout or restart might be necessary. >>>\n"
